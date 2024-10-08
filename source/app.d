f5dbbd4b81f97bdf7ce8aaf4d517fa295b80138c
cbleser <cr@tagion.org>
2024-10-08 17:26:47 +0300

    Done: Sample raylib

--- source/app.d
+++ source/app.d
@@ -1,6 +1,17 @@
-import std.stdio;
+import raylib;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+    // call this before using raylib
+    validateRaylibBinding();
+    InitWindow(800, 600, "Hello, Raylib-D!");
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(Colors.RAYWHITE);
+        DrawText("Hello, World!", 400, 300, 28, Colors.BLACK);
+        EndDrawing();
+    }
+    CloseWindow();
 }
